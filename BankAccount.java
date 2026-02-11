60a2c7e5c41815324cb87f586ba466b233110d78
Prathamesh Chidrewar <prathamesh.chidrewar2004@gmail.com>
2026-02-11 19:42:03 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,33 @@
+class BankAccount {
+    private double balance;
+
+    // Constructor
+    BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    void displayBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount(1000);
+        acc.deposit(500);
+        acc.withdraw(300);
+        acc.displayBalance();
+    }
+}
